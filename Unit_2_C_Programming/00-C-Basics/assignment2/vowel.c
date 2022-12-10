ce94e3db68991fa84466cfeaaa7af9843e5da7e5
Sarah <sarah.hesham.sh@gmail.com>
2022-12-11 01:33:05 +0200

    vowels

---
+++ Unit_2_C_Programming/00-C-Basics/assignment2/vowel.c
@@ -0,0 +1,21 @@
+/*
+ * vowel.c
+ *
+ *  Created on: Dec 11, 2022
+ *      Author: Sarah
+ *      Checking whether the alphabet entered is a vowel or not
+ */
+#include "stdio.h"
+int main ()
+{
+	char a;
+	printf("Enter an alphabet:");
+	fflush(stdout);
+	scanf("%c",&a);
+	fflush(stdin);
+	if((a=='a')||(a=='e')||(a=='i')||(a=='u')||(a=='o'))
+		printf("%c is a vowel", a);
+	else printf("%c is not a vowel", a);
+	return 0;
+}
+
