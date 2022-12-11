9fc6eac1fe043624e09c68e5c41e89345694acfe
Sarah <sarah.hesham.sh@gmail.com>
2022-12-11 02:51:35 +0200

    swapping_without_temp

---
+++ Unit_2_C_Programming/00-C-Basics/assignment1/ex7.c
@@ -0,0 +1,25 @@
+/*
+ * swap.c
+ *
+ *  Created on: Dec 11, 2022
+ *      Author: Sarah
+ *      Swapping two numbers
+ */
+#include "stdio.h"
+int main()
+{
+	float a,b;
+	printf("Enter value for a:");
+	fflush(stdout);
+	scanf("%f",&a);
+	fflush(stdin);
+	printf("Enter value for b:");
+	fflush(stdout);
+	scanf("%f",&b);
+	fflush(stdin);
+	printf("a=%f",b);
+	printf("b=%f",a);
+
+	return 0;
+}
+
